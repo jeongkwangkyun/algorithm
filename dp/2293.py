72ec89083fdd957c26807c11ebc2f7a5346d4481
jeongkwangkyun <rhkdrbs06@naver.com>
2021-03-16 00:22:15 +0900

    동전 1 DP

---
+++ dp/2293.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n,k= map(int,input().split())
+dp=[0 for _ in range(k+1)]
+dp[0]=1
+
+coins=[int(input()) for _ in range(n)]
+
+for i in coins:
+    for j in range(1,k+1):
+        if j-i >= 0:
+            dp[j]+=dp[j-i]
+print(dp[k])
