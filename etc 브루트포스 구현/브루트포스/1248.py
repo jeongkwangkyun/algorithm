0b711347d1f876de556d571ee1048b385e17fb1f
Jeong Kwangkyun <rhkdrbs06@naver.com>
2021-06-03 18:34:34 +0900

    맞춰봐 BF,재귀

---
+++ etc 브루트포스 구현/브루트포스/1248.py
@@ -0,0 +1,51 @@
+# 맞춰봐
+# 재귀가 너무 어려워 ㅜㅜ
+# s에 -,0,+를 구분해두고 -는 -10~-1 +는 1~10 해서 경우의 수를 줄이고
+# result idx별로 둬서 solve에서 인덱스 값 check에서 더한 값 비교
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+
+digit=list(input())
+
+s=[[0]*n for _ in range(n)]
+
+def check(idx):
+    hap=0
+    for i in range(idx,-1,-1):
+        hap+=result[i]
+        if hap == 0 and s[i][idx]!=0:
+            return False
+        elif hap<0 and s[i][idx]>=0:
+            return False
+        elif hap>0 and s[i][idx]<=0:
+            return False
+    return True
+
+def solve(idx):
+    if idx==n:
+        return True
+
+    if s[idx][idx]==0:
+        result[idx]=0
+        return solve(idx+1)
+
+    for i in range(1,11):
+        result[idx]=s[idx][idx]*i
+        if check(idx) and solve(idx+1):
+            return True
+    return False
+
+for i in range(n):
+    for j in range(i,n):
+        temp=digit.pop(0)
+        if temp=='+':
+            s[i][j]=1
+        elif temp=='-':
+            s[i][j]=-1
+
+result=[0]*n
+solve(0)
+print(' '.join(map(str, result)))
