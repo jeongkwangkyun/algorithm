f58f315d75feb4ed50a88bf5285310e88d148d63
정광균 <rhkdrbs06@naver.com>
2022-03-10 15:14:24 +0900

    Add : 220310 힙

---
+++ data structure/Main_bj_s2_최대힙_11927.java
@@ -0,0 +1,27 @@
+package b0310;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_s2_최대힙_11927 {
+	static int N;
+	static PriorityQueue<Integer> queue;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		queue = new PriorityQueue<Integer>((o1 , o2) -> o1 - o2);
+		
+		for(int i = 0 ; i < N ; i ++) {
+			int input = Integer.parseInt(br.readLine());
+			
+			if(input > 0) queue.add(input);
+			else {
+				if(queue.isEmpty()) sb.append(0).append("\n");
+				else sb.append(queue.poll()).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
