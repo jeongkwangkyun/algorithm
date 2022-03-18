a6cb93f6369b6c4de387eada7afc34b7a2a17e2d
정광균 <rhkdrbs06@naver.com>
2022-03-18 22:13:56 +0900

    Add : 220318 map

--- data structure/Main_bj_s4_1920_수찾기.java
+++ data structure/Main_bj_s4_1920_수찾기.java
@@ -3,32 +3,31 @@ package b0317;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main_bj_s4_1920_수찾기 {
 	static int N,M;
-	static int[] nList, mList;
+	static HashMap<Integer, Integer> hashMap = new HashMap<>();
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		nList = new int[100001];
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0 ; i < N ; i++) {
 			int tmp = Integer.parseInt(st.nextToken());
-			nList[tmp] = 1; 
+			hashMap.put(tmp, 1); 
 		}
 		
 		M = Integer.parseInt(br.readLine());
-		mList = new int[N];
+		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0 ; i < M ; i++) {
-			mList[i] = Integer.parseInt(st.nextToken());
+			int input = Integer.parseInt(st.nextToken());
+			if(hashMap.containsKey(input)) sb.append(1).append("\n");
+			else sb.append(0).append("\n");
 		}
 		
-		
-		for(int i = 0 ; i < M ; i++) {
-			if(nList[mList[i]] == 1) System.out.println(1);
-			else System.out.println(0);
-		}
+		System.out.println(sb);
 	}
 }
