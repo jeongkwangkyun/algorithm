411ee1913e8f211b63fb36e71ba9f3d2dd5ed660
정광균 <rhkdrbs06@naver.com>
2022-04-16 13:51:36 +0900

    Add : 220416 dp or 수학

---
+++ dp/Main_bj_s5_9655_돌게임.java
@@ -0,0 +1,17 @@
+package b0415;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_s5_9655_돌게임 {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		if(N % 2 == 1) System.out.println("SK");
+		else System.out.println("CY");
+		
+	}
+
+}
